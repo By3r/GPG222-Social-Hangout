91f87a66277f45afb2123fd8923808ca9429c944
BYER <tetekim31@gmail.com>
2025-02-19 00:30:31 +0400

    Starting with chat system logic. wrote brief pseudo to continue off of tommorow

--- Assets/GPG222_SocialHang_2025/Scripts/Player/Interactions/ChatUIManager.cs
+++ Assets/GPG222_SocialHang_2025/Scripts/Player/Interactions/ChatUIManager.cs
@@ -1,16 +1,32 @@
+using System.Net.Sockets;
+using TMPro;
 using UnityEngine;
 
 public class ChatUIManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    #region Variables
+    [SerializeField] private int port;
+    [SerializeField] private GameObject chatPopup;
+    [SerializeField] private TMP_Text chatMessageDisplayer;
+    [SerializeField] private TMP_InputField messageInputField;
+    [SerializeField] private string iPAddress;
+    private Socket _server;
+    private Socket _client;
+    #endregion
+
+    private void Start()
     {
-        
+        // initialise the socket.
+        // link the socket to an ip endpoint.
+        // create a queue
+        // sett false to socket blocking, lol.
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // buffer time to encode unicode for emojis lmao
+    // get the input field's content and encode it into a buffer.
+    // send the buffer to the client socket.
+    // display recieved buffer in another script. note: i'll be changing the script's name to be more specifuc than a general manager.
+    // ++ Note: have a server mono, 
+
 }
