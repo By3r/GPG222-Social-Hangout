0b49f99b6a52e2adaf4461fdb0d356fbb1af0932
JanWillem Goedvolk <caringsharing57@gmail.com>
2025-03-19 09:50:51 +0400

    Tested Client connection to Server (Success!)

---
+++ Assets/GPG222SocialHangout25T1/Scripts/Packets/BasePacket.cs
@@ -0,0 +1,67 @@
+using System.Drawing;
+using System.IO;
+using Networking.Core;
+
+namespace Networking.Packets
+{
+    public class BasePacket
+    {
+        protected MemoryStream  _readerStream;
+        protected MemoryStream  _writerStream;
+        protected BinaryReader _reader;
+        protected BinaryWriter _writer;
+        public int Size { get; protected set; }
+
+        public enum PacketType
+        {
+            None = 0,
+            Join = 1,
+            ClientList = 2,
+            Message = 3
+        }
+        public PacketType Type { get; set; }
+        public PlayerData PlayerData { get; set; }
+
+        public BasePacket()
+        {
+            Type = PacketType.None;
+            PlayerData = null;
+        }
+
+        public BasePacket(PacketType type, PlayerData playerData)
+        {
+            Type = type;
+            PlayerData = playerData;
+        }
+
+        protected void BeginSerialize()
+        {
+            _writerStream = new MemoryStream();
+            _writer = new BinaryWriter(_writerStream);
+            
+            _writer.Write((int)Type);
+            _writer.Write(PlayerData.DuckID);
+            _writer.Write(PlayerData.Username);
+        }
+
+        protected byte[] EndSerialize()
+        {
+            return _writerStream.ToArray();
+        }
+
+        public void Deserialize(byte[] buffer, ref int count, ref int offset)
+        {
+            _readerStream = new MemoryStream(buffer);
+            _readerStream.Seek(offset, SeekOrigin.Begin);
+            _reader = new BinaryReader(_readerStream);
+            Size = 0; // The size of the packet so far being deserialized
+            
+            Type = (PacketType)_reader.ReadInt32();
+            Size += sizeof(int); // PacketType size
+            
+            PlayerData = new PlayerData(_reader.ReadInt32(), _reader.ReadString());
+            Size += PlayerData.Username.Length + 1; // Full size of the string including null terminator
+            Size += sizeof(int); // DuckID size
+        }
+    }
+}
---
+++ Assets/GPG222SocialHangout25T1/Scripts/Packets/JoinPacket.cs
@@ -0,0 +1,38 @@
+using Networking.Core;
+
+namespace Networking.Packets
+{
+    /// <summary>
+    /// This packet will send the PlayerData of a client who has just joined the lobby
+    /// </summary>
+    public class JoinPacket : BasePacket
+    {
+        public JoinPacket()
+        {
+            Type = BasePacket.PacketType.Join;
+            PlayerData = null;
+        }
+        public JoinPacket(PlayerData playerData)
+        {
+            Type = PacketType.Join;
+            PlayerData = playerData;
+        }
+
+        public byte[] Serialize()
+        {
+            BeginSerialize();
+            return EndSerialize();
+        }
+
+        public new JoinPacket Deserialize(byte[] buffer, ref int bufferSize, ref int offset)
+        {
+            base.Deserialize(buffer, ref bufferSize, ref offset);
+            
+            // Update the buffer size and offset from the calculated size
+            bufferSize -= Size;
+            offset += Size;
+            
+            return this;
+        }
+    }
+}
