fefdf3f97e58ff325035a1de15dc3c95f3ab4141
Jan Willem Goedvolk <jan.goedvolk@student.saedubai.com>
2025-05-03 13:35:39 +0400

    Starting Using Physics To Sync Objects

--- Assets/GPG222SocialHangout25T1/Scripts/NetworkComponent.cs
+++ Assets/GPG222SocialHangout25T1/Scripts/NetworkComponent.cs
@@ -11,6 +11,8 @@ namespace Networking.Core
     {
         public string GameObjectID { get; private set; }
         public int OwnerID { get; private set; }
+        public bool IsPhysics = false;
+        private Rigidbody _rb;
 
         [SerializeField] private float _packetFrequency = .2f;
         private float _packetTimer = 0f;
@@ -20,6 +22,7 @@ namespace Networking.Core
         {
             Client.Instance.PositionPacketReceivedEvent += PositionPacketReceivedEvent;
             _syncTransform = transform;
+            _rb = GetComponent<Rigidbody>();
         }
 
         private void OnDisable()
@@ -31,24 +34,21 @@ namespace Networking.Core
         {
             if (Client.Instance.PlayersInLobby.Count > 1) // We are not the only client in the server
             {
-                // we don't want to send  updates in the lobby scene if we are a duck (bouyancy components are present)
-                if (Client.Instance.SceneIndex == 1) // We are in the lobby scene
-                {
-                    // We don't continue if we have a bouyancy controller, ie. we are a player
-                    BouyancyObject bouyancyObject = GetComponent<BouyancyObject>();
-                    if (bouyancyObject != null)
-                    {
-                        return;
-                    }
-                }
-                
                 if (OwnerID != Client.Instance.PlayerData.DuckID) // This is not our object so it should have its transform synced
                 {
                     // If we have a transform to sync to, then lerp to it
                     if (_syncTransform != null)
                     {
-                        transform.position = Vector3.Lerp(transform.position, _syncTransform.position,  _packetFrequency);
-                        transform.rotation = Quaternion.Lerp(transform.rotation, _syncTransform.rotation, _packetFrequency);
+                        if (IsPhysics)
+                        {
+                            _rb.velocity = Vector3.Lerp(_rb.velocity, _syncTransform.position, _packetFrequency);
+                            _rb.angularVelocity = Vector3.Lerp(_rb.angularVelocity, _syncTransform.rotation.eulerAngles, _packetFrequency);
+                        }
+                        else
+                        {
+                            transform.position = Vector3.Lerp(transform.position, _syncTransform.position, _packetFrequency);
+                            transform.rotation = Quaternion.Lerp(transform.rotation, _syncTransform.rotation, _packetFrequency);
+                        }
                     }
                     
                     return; // We only want to continue if this is our object
@@ -60,8 +60,19 @@ namespace Networking.Core
                 {
                     _packetTimer = 0f;
                     // TODO: Small optimisation is theoretically possible by only sending position packets if we have moved a sufficient distance
-                    PositionPacket pp = new PositionPacket(Client.Instance.GetPlayerData(OwnerID),  GameObjectID, transform.position, transform.rotation);
-                    Client.Instance.SendPositionPacket(pp);
+                    if (IsPhysics)
+                    {
+                        Vector3 av = _rb.angularVelocity;
+                        Quaternion rotation = new Quaternion(av.x, av.y, av.z, 0);
+                        PositionPacket pp = new PositionPacket(Client.Instance.GetPlayerData(OwnerID),  GameObjectID, _rb.velocity, rotation, true);
+                        Client.Instance.SendPositionPacket(pp);
+                    }
+                    else
+                    {
+                        PositionPacket pp = new PositionPacket(Client.Instance.GetPlayerData(OwnerID),  GameObjectID, transform.position, transform.rotation);
+                        Client.Instance.SendPositionPacket(pp);
+                    }
+                    
                 }
                 
                 
@@ -74,6 +85,7 @@ namespace Networking.Core
             {
                 if (GameObjectID == pp.ObjectID)
                 {
+                    IsPhysics = pp.IsPhysics;
                     _syncTransform.position = pp.Position;
                     _syncTransform.rotation = pp.Rotation;
                 }
--- Assets/GPG222SocialHangout25T1/Scripts/Packets/PositionPacket.cs
+++ Assets/GPG222SocialHangout25T1/Scripts/Packets/PositionPacket.cs
@@ -9,6 +9,7 @@ namespace Networking.Packets
         public string ObjectID { get; private set; }
         public Vector3 Position { get; private set; }
         public Quaternion Rotation { get; private set; }
+        public bool IsPhysics = false;
 
         public PositionPacket() : base(PacketType.Position)
         {
@@ -16,15 +17,17 @@ namespace Networking.Packets
             ObjectID = "";
             Position = new Vector3(0, 0, 0);
             Rotation = Quaternion.identity;
+            IsPhysics = false;
         }
 
-        public PositionPacket(PlayerData playerData, string objectID, Vector3 position, Quaternion rotation) : base(PacketType.Position,
+        public PositionPacket(PlayerData playerData, string objectID, Vector3 position, Quaternion rotation, bool isPhysics = false) : base(PacketType.Position,
             playerData)
         {
             OwnerID = playerData.DuckID;
             ObjectID = objectID;
             Position = position;
             Rotation = rotation;
+            IsPhysics = isPhysics;
         }
 
         public byte[] Serialize()
@@ -39,6 +42,7 @@ namespace Networking.Packets
             _writer.Write(Rotation.y);
             _writer.Write(Rotation.z);
             _writer.Write(Rotation.w);
+            _writer.Write(IsPhysics);
             return EndSerialize();
         }
 
@@ -54,6 +58,9 @@ namespace Networking.Packets
             Position = new Vector3(_reader.ReadSingle(), _reader.ReadSingle(), _reader.ReadSingle());
             Rotation = new Quaternion(_reader.ReadSingle(), _reader.ReadSingle(), _reader.ReadSingle(), _reader.ReadSingle());
             Size += sizeof(float) * 7;
+            
+            IsPhysics = _reader.ReadBoolean();
+            Size += sizeof(bool);
 
             bufferSize -= Size;
             offset += Size;
